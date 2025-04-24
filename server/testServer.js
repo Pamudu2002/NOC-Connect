a99780b7c291d57cc3b1e7477fff32030f43f752
Geesan Munasinghe <sugithgeesan@gmail.com>
2025-04-24 23:50:26 +0530

    connected

--- server/testServer.js
+++ server/testServer.js
@@ -12,7 +12,8 @@ const port = 5000;
 app.use(express.json()); 
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  // origin: `*`, 
+  origin: `http://localhost:3000`, 
   credentials: true, 
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type'],
